d873d91e07109c595d2fd3d73f25254948ba02ba
Renato Athaydes <renato@athaydes.com>
2022-01-06 22:42:33 +0100

    Initial commit.

---
+++ src/main/java/demo/JavaType.java
@@ -0,0 +1,7 @@
+package demo;
+
+public class JavaType {
+    public String getMessage() {
+        return "hello world";
+    }
+}
---
+++ src/main/java/demo/Main.java
@@ -0,0 +1,26 @@
+package demo;
+
+import org.graalvm.polyglot.Context;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length > 1) throw new RuntimeException("Please provide JS source code to run as the only argument");
+
+        var jsCode = "print('Calling Java type method');\n" +
+                "print('Got Java message: ' + javaType.getMessage());\n" +
+                "'done';";
+
+        if (args.length == 1) {
+            jsCode = args[0];
+        }
+
+        var jsEngine = Context.newBuilder("js")
+                .allowAllAccess(true)
+                .allowNativeAccess(true)
+                .build();
+
+        jsEngine.getBindings("js").putMember("javaType", new JavaType());
+
+        System.out.println(jsEngine.eval("js", jsCode));
+    }
+}
