9ccdd652f4913029d637fd52a4d73fe1a88db9df
Renato Athaydes <renato@athaydes.com>
2022-01-06 23:12:14 +0100

    Add some stuff to JavaType for experimentation.

--- src/main/java/demo/pub/JavaType.java
+++ src/main/java/demo/pub/JavaType.java
@@ -1,7 +1,18 @@
 package demo.pub;
 
 public class JavaType {
+
+    public final String stringField = "public field";
+
     public String getMessage() {
         return "hello world";
+    }
+
+    public boolean getTrue() {
+        return true;
+    }
+
+    private String privateMethod() {
+        return "should not be able to call me";
     }
 }
