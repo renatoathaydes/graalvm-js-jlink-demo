7673773a57f4686fc77f5285fb266fa247ac2cd7
Renato Athaydes <renato@athaydes.com>
2022-01-06 23:11:55 +0100

    Add options to run file or String.

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -2,16 +2,25 @@ package demo;
 
 import demo.pub.JavaType;
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Source;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        if (args.length > 1) throw new RuntimeException("Please provide JS source code to run as the only argument");
-
-        var jsCode = "print('Calling Java type method');\n" +
+    public static void main(String[] args) throws IOException {
+        File scriptFile = null;
+        String jsCode = "print('Calling Java type method');\n" +
                 "print('Got Java message: ' + javaType.getMessage());\n" +
                 "'done';";
 
-        if (args.length == 1) {
+        if (args.length == 2 && args[0].equals("-f")) {
+            scriptFile = new File(args[1]);
+        } else if (args.length > 1) {
+            throw new RuntimeException("Options are:\n" +
+                    "  -f <js-script-file>\n" +
+                    "  <js-script>");
+        } else if (args.length == 1) {
             jsCode = args[0];
         }
 
@@ -22,6 +31,14 @@ public class Main {
 
         jsEngine.getBindings("js").putMember("javaType", new JavaType());
 
-        System.out.println(jsEngine.eval("js", jsCode));
+        Source source;
+
+        if (scriptFile != null) {
+            source = Source.newBuilder("js", scriptFile).build();
+        } else {
+            source = Source.create("js", jsCode);
+        }
+
+        System.out.println(jsEngine.eval(source));
     }
 }
